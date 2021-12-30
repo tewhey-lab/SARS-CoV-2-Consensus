ca8081e52259fd3a7bc32176d54307adf6f3391c
Hannah Dewey <hannah.dewey@jax.org>
2021-12-30 12:38:35 -0500

    Update sars_cov_2_consensus.sh

--- sars_cov_2_consensus.sh
+++ sars_cov_2_consensus.sh
@@ -20,7 +20,14 @@ echo "samtools fastq -1 mapping/${ID}_filtered.R1.fq -2 mapping/${ID}_filtered.R
 
 # echo "bwa mem -k 12 -B 1 -t 12 $REF mapping/${ID}_filtered.R1.fq mapping/${ID}_filtered.R2.fq | samtools view -u - | samtools sort -O BAM -o mapping/${ID}.bam -" >> slurm/slurm.${ID}.runCMD.sh
 echo "minimap2 -K 20M -x sr -a $REF mapping/${ID}_filtered.R1.fq mapping/${ID}_filtered.R2.fq | samtools view -u -h -F 4 - | samtools sort -O BAM -o mapping/${ID}.bam -" >> slurm/slurm.${ID}.runCMD.sh
-echo "/projects/tewhey-lab/rtewhey/COVID/bin/samtools/samtools ampliconclip --both-ends --strand  --filter-len 20  --no-excluded -b /projects/tewhey-lab/projects/COVID/reference_files/artic_primers_v4.1.bed mapping/${ID}.bam |samtools view -u - | samtools sort -O BAM -o mapping/${ID}.clipped.bam" >> slurm/slurm.${ID}.runCMD.sh
+
+##Remove soft-clipped reads
+echo "samtools view mapping/${ID}.bam | awk '(\$6 ~ /H|S/){print \$1}' | sort -k1,1 | uniq > mapping/${ID}_names.txt" >> slurm/slurm.${ID}.runCMD.sh
+echo "picard FilterSamReads -Xmx3g I=mapping/${ID}.bam O=mapping/${ID}_ClipRemoved.bam READ_LIST_FILE=mapping/${ID}_names.txt FILTER=excludeReadList" >>slurm/slurm.${ID}.runCMD.sh
+
+echo "/projects/tewhey-lab/rtewhey/COVID/bin/samtools/samtools ampliconclip --both-ends --strand --tolerance 4 --filter-len 20  --no-excluded -b /projects/tewhey-lab/projects/COVID/reference_files/artic_primers_v4.1.bed mapping/${ID}_ClipRemoved.bam > mapping/${ID}.clipped.uncorr.sam" >> slurm/slurm.${ID}.runCMD.sh
+echo "samtools calmd mapping/${ID}.clipped.uncorr.sam /projects/tewhey-lab/projects/COVID/reference_files/NC_045512.fa | samtools view -u - | samtools sort -O BAM -o mapping/${ID}.clipped.bam" >> slurm/slurm.${ID}.runCMD.sh
+echo "rm mapping/${ID}.clipped.uncorr.sam" >> slurm/slurm.${ID}.runCMD.sh
 echo "samtools index mapping/${ID}.clipped.bam" >> slurm/slurm.${ID}.runCMD.sh
 
 echo "samtools view -b mapping/${ID}.clipped.bam | genomeCoverageBed -d -ibam stdin > QC/${ID}.hist" >> slurm/slurm.${ID}.runCMD.sh
@@ -29,6 +36,6 @@ echo "Rscript /projects/tewhey-lab/projects/COVID/scripts/coverage_hist.R QC/${I
 echo "samtools mpileup -A -d 0 -Q 0 -B mapping/${ID}.clipped.bam | ivar consensus -t 0 -p working_consensus/${ID}.consensus" >> slurm/slurm.${ID}.runCMD.sh
 echo "samtools mpileup -A -d 0 -Q 0 --reference $REF mapping/${ID}.clipped.bam | ivar variants -g ${REF%%.fa}.gff -r $REF -p working_consensus/${ID}.consensus -t 0.05" >> slurm/slurm.${ID}.runCMD.sh
 echo "python /projects/tewhey-lab/projects/COVID/scripts/trim_ends_fasta.py working_consensus/${ID}.consensus.fa 80 working_consensus/${ID}.consensus.trimmed" >> slurm/slurm.${ID}.runCMD.sh
-done < 20210924_samples_list.txt
+done < 20210303_samples_list.txt
 
 for i in `ls slurm/*runCMD.sh`; do echo $i; sbatch $i; done
